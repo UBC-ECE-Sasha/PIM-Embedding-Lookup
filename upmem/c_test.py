f477818699f1c52fe1c86be1bd7246fb7308e327
Niloofar <nzarif@ece.ubc.ca>
2020-05-01 14:23:11 -0700

    DPU code completed

--- upmem/c_test.py
+++ upmem/c_test.py
@@ -5,13 +5,11 @@ so_file="/root/upmem/emblib.so"
 
 my_functions=CDLL(so_file)
 
-my_functions.populate_mram.argtypes = c_uint32, c_uint8, c_uint8, POINTER(c_uint32)
+my_functions.populate_mram.argtypes = c_uint32, c_uint8, c_uint8, POINTER(c_float)
 my_functions.populate_mram.restype= None
-p=(c_uint32*20)()
+p=(c_float*20)(1.0,2.0,3.0,4.0,5.0,2.0,4.0,6.0,8.0,10.0,3.0,6.0,9.0,12.0,15.0,4.0,8.0,12.0,16.0,20.0)
 my_functions.populate_mram(1,4,5,p)
-#set_pointer=my_functions.populate_mram(1,4, 5, {1,2,3,4,5,2,4,6,8,10,3,6,9,12,15,4,8,12,16,20})
 my_functions.lookup.argtypes= POINTER(c_uint32), c_uint8, c_uint8, c_uint8
 my_functions.lookup.restype= None
-q=(c_uint32*4)()
+q=(c_uint32*4)(1,2,0,3)
 my_functions.lookup(q,4,5,4)
-
--- upmem/emb_host.c
+++ upmem/emb_host.c
@@ -11,65 +11,70 @@
 
 struct dpu_set_t set;
 
-// to build the code: gcc -O3 --std=c99 -o emb_host emb_host.c -g `dpu-pkg-config --cflags --libs dpu`
+// to compile the code: gcc -O0 -g3 --std=c99 -o emb_host emb_host.c -g `dpu-pkg-config --cflags --libs dpu`
+// to build a shared library: gcc -shared -Wl,-soname,emb_host -o emblib.so -fPIC emb_host.c `dpu-pkg-config --cflags --libs dpu`
 
-void *populate_mram(uint32_t dpu_n,uint8_t nr_rows, uint8_t nr_cols, uint32_t *data) {
+void populate_mram(uint32_t dpu_n,uint8_t nr_rows, uint8_t nr_cols, float *data) {
 
     //uint32_t buffer[nr_rows][nr_cols];
     struct dpu_set_t dpu;
+    uint8_t write_len=nr_cols*nr_rows*sizeof(float);
+    if(write_len%8!=0)
+        write_len+=8-(write_len%8);
 
     DPU_ASSERT(dpu_alloc(dpu_n, NULL, &set));
-    DPU_ASSERT(dpu_load(set, "emb_dpu_lookup", NULL));
+    DPU_ASSERT(dpu_load(set, DPU_BINARY, NULL));
 
     DPU_ASSERT(dpu_copy_to(set, "row_size_input", 0, (const uint8_t *)&nr_rows, sizeof(nr_rows)));
     DPU_ASSERT(dpu_copy_to(set, "col_size_input", 0, (const uint8_t *)&nr_cols, sizeof(nr_cols)));
     
-    DPU_ASSERT(dpu_copy_to(set, DPU_MRAM_HEAP_POINTER_NAME, 0, (const uint32_t *)data, nr_cols*nr_rows*sizeof(uint32_t)));
+    DPU_ASSERT(dpu_copy_to(set, DPU_MRAM_HEAP_POINTER_NAME, 0, (const float *)data, write_len));
 
-    //printf("%d\n",data[0]);
-    return (void *)&set;
+    printf("last data=%f\n",data[14]);
+    return;
 }
 
-void lookup(uint32_t* input,uint8_t length, uint8_t nr_cols, uint8_t nr_rows){
-    uint32_t ans[length*nr_cols];
+void lookup(float* input,uint8_t length, uint8_t nr_cols, uint8_t nr_rows){
+    float ans[length*nr_cols];
+
+    uint8_t write_len=length*sizeof(float);
+    if(write_len%8!=0)
+        write_len+=8-(write_len%8);
 
     DPU_ASSERT(dpu_copy_to(set, "index_len_input", 0, (const uint8_t *)&length, sizeof(length)));
-    DPU_ASSERT(dpu_copy_to(set, DPU_MRAM_HEAP_POINTER_NAME, nr_cols*nr_rows*sizeof(uint32_t), (const uint32_t *)input, length*sizeof(uint32_t)));
+    DPU_ASSERT(dpu_copy_to(set, DPU_MRAM_HEAP_POINTER_NAME, nr_cols*nr_rows*sizeof(float), (const float *)input, write_len));
 
     DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
 
     struct dpu_set_t dpu;
     DPU_FOREACH(set, dpu) {
-        DPU_ASSERT(dpu_copy_from(dpu, DPU_MRAM_HEAP_POINTER_NAME, (uint32_t)(nr_cols*nr_rows*sizeof(uint32_t)) , ans, sizeof(uint32_t)*nr_cols*length));
+        DPU_ASSERT(dpu_copy_from(dpu, DPU_MRAM_HEAP_POINTER_NAME, (float)(nr_cols*nr_rows*sizeof(float)) , ans, sizeof(float)*nr_cols*length));
         DPU_ASSERT(dpu_log_read(dpu, stdout));
-        for (int i=0;i<length;i++){
-            for (int j=0; j<nr_cols;j++)
-	            printf("ans[%d][%d] = %d\n",input[i],j, ans[i*nr_cols+j]);
-	    }
+        //for (int i=0;i<length;i++){
+            //for (int j=0; j<nr_cols;j++)
+	           // printf("ans[%d][%d] = %f\n",(uint32_t)input[i],j, ans[i*nr_cols+j]);
+	    //}
     }
     DPU_ASSERT(dpu_free(set));
 }
 
-int init_dpu(uint32_t dpu_n){
-    struct dpu_set_t set;
-    printf("n:%d\n",dpu_n);
-    DPU_ASSERT(dpu_alloc(dpu_n, NULL, &set));
-    DPU_ASSERT(dpu_load(set, "emb_dpu_lookup", NULL));
+int init_dpu(){
+    //printf("size:%d\n",(unsigned int)sizeof(float));
 
-    uint32_t data[20]={
-        1,2,3,4,5,
-        2,4,6,8,10,
-        3,6,9,12,15,
-        4,8,12,16,20
+    float data[15]={
+        1.1,2.2,3.3,4.4,5.5,
+        2.2,4.4,6.6,8.8,10.10,
+        3.3,6.6,9.9,12.12,15.15,
     };
-    uint32_t input[5]={0,1,2,3};
-    uint8_t length=4;
+    float input[2]={1.0,2.0};
+    uint8_t length=2;
     
-    //populate_mram(set,4,5,data);
-    //lookup(set,input,length,5,4);
+    populate_mram(1,3,5,data);
+    lookup(input,length,5,3);
     return 0;
 }
 
 int main(){
+    init_dpu(1);
     return 0;
 }
