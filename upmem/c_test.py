e7e0979b3cd41223b6911d976f91d9fa021a6a1f
Amirabbas Kashanipour <amir.akp1@gmail.com>
2020-06-12 12:42:24 -0700

    seg fault fixed

--- upmem/c_test.py
+++ upmem/c_test.py
@@ -1,7 +1,7 @@
 from ctypes import *
 import numpy
 
-so_file="/home/upmem0016/abbask/emb/emblib.so"
+so_file="/home/upmem0016/abbask/seg/emblib.so"
 
 my_functions=CDLL(so_file)
 
@@ -16,7 +16,7 @@ my_functions.populate_mram(4,5,p)
 
 my_functions.lookup.argtypes= POINTER(c_double), POINTER(c_uint64), c_uint64, c_uint64, c_uint64
 my_functions.lookup.restype= None
-tmp=[0.0, 0.0, 0.0, 0.0, 0.0]
-ans=(c_double*5)(*tmp)
-q=(c_uint64*3)(0,2,1,)
-my_functions.lookup(ans,q,3,4,5)
+tmp=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+ans=(c_double*20)(*tmp)
+q=(c_uint64*4)(0,1,2,3)
+my_functions.lookup(ans,q,4,4,5)
