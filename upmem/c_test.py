155b46377e684f9803f4ee23d5de6448f70dba8c
Niloofar Zarif <niloo@upmemcloud5.cloud.upmem.com>
2020-10-13 01:19:18 +0200

    changed the values of data_ptr to int

--- upmem/c_test.py
+++ upmem/c_test.py
@@ -9,7 +9,7 @@ my_functions=CDLL(so_file)
 
 my_functions.populate_mram.argtypes = c_uint32, c_uint64, c_uint64, POINTER(c_int32)
 my_functions.populate_mram.restype= None
-data_ptr=(c_int32*20)(1.0,2.0,3.0,4.0,5.0,2.0,4.0,6.0,8.0,10.0,3.0,6.0,9.0,12.0,15.0,4.0,8.0,12.0,16.0,20.0)
+data_ptr=(c_int32*20)(1,2,3,4,5,2,4,6,8,10,3,6,9,12,15,4,8,12,16,20)
 my_functions.populate_mram(0,4,5,data_ptr)
 
 #lookups
