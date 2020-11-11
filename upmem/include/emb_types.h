596f0410d3182514eb947220f1f303ebfe82bac1
Niloofar Zarif <niloo@upmemcloud3.cloud.upmem.com>
2020-11-11 23:21:51 +0100

    moved emb_types.h to include

---
+++ upmem/include/emb_types.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MAX_ENC_BUFFER_SIZE MEGABYTE(MAX_ENC_BUFFER_MB)
+#define MAX_CAPACITY MEGABYTE(14) //Must be a multiply of 2
+#define NR_TABLES 8
+#define DPUS_PER_RANK 64
+#define AVAILABLE_RANKS 10
+#define MAX_NR_BUFFERS 100
+#define NR_COLS 5
+
+struct embedding_buffer {
+    int32_t *data;
+    uint64_t first_row, last_row;
+    uint32_t table_id;
+};
+
+struct embedding_table {
+    uint32_t first_dpu_id, last_dpu_id, nr_buffers;
+    uint64_t nr_rows;
+    struct embedding_buffer **buffers;
+    int32_t *ans;
+};
+
+struct lookup_result {
+    int32_t data[NR_COLS];
+    uint32_t id;
+    bool is_complete;
+};
