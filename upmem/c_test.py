0d820993d9f2a6d29763e6026d8e8d9545436d61
Niloofar Zarif <niloo@upmemcloud5.cloud.upmem.com>
2020-12-03 02:52:07 +0100

    made toy test larger

--- upmem/c_test.py
+++ upmem/c_test.py
@@ -5,11 +5,13 @@ from ctypes import *
 so_file = None
 my_functions = None
 num_dpu= 8
+num_batches= 32
 # Defaults, can be overriden by cli arguments
 if __name__ != "__main__":
     so_file = "./emblib.so"
     my_functions = CDLL(os.path.abspath(so_file))
-    num_dpu= 8
+    num_dpu= 12
+    num_batches= 32
 
 
 def parse():
@@ -26,7 +28,7 @@ def parse():
 def populate():
     my_functions.populate_mram.argtypes = c_uint32, c_uint64, POINTER(c_int32)
     my_functions.populate_mram.restype = None
-    data_ptr = (c_int32 * 20)(1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20)
+    data_ptr = (c_int32 * 24)(1, 2, 3, 4, 5, 6, 2, 4, 6, 8, 10, 12, 3, 6, 9, 12, 15, 18, 4, 8, 12, 16, 20, 24)
     for i in range (0, num_dpu):
         my_functions.populate_mram(i, 4, data_ptr)
 
@@ -34,12 +36,23 @@ def populate():
 def lookup():
     my_functions.lookup.argtypes = POINTER(c_uint32), POINTER(c_uint32), POINTER(c_uint64), POINTER(c_uint64), POINTER(c_int32)
     my_functions.lookup.restype = None
-    indices=(c_uint32 * 24)(1,3,2,1,3,2,1,3,2,1,3,2,1,3,2,1,3,2,1,3,2,1,3,2)
-    offsets=(c_uint32 * 16)(0,2,0,2,0,2,0,2,0,2,0,2,0,2,0,2)
-    indices_len=(c_uint64 * 8)(3,3,3,3,3,3,3,3)
-    offsets_len=(c_uint64 * 8)(2,2,2,2,2,2,2,2)
-    ans=(c_int32 * 80)()
-    my_functions.lookup(indices, offsets, indices_len, offsets_len,ans)
+    indices=[]
+    offsets=[]
+    indices_len=[]
+    offsets_len=[]
+    for i in range(0,num_dpu):
+        for j in range(0,num_batches):
+            indices.extend([1,3,2,0])
+            offsets.append(j*4)
+        indices_len.append(4*num_batches)
+        offsets_len.append(num_batches)
+
+    indices_ptr=(c_uint32 * len(indices))(*indices)
+    offsets_ptr=(c_uint32 * len(offsets))(*offsets)
+    indices_len_ptr=(c_uint64 * len(indices_len))(*indices_len)
+    offsets_len_ptr=(c_uint64 * len(offsets_len))(*offsets_len)
+    ans=(c_int32 * (num_batches*6*num_dpu))()
+    my_functions.lookup(indices_ptr, offsets_ptr, indices_len_ptr, offsets_len_ptr,ans)
 
 
 if __name__ == "__main__":
