b49bbe158c62cccd28e8a178cb2bb51278c14dbe
Niloofar Zarif <niloo@upmemcloud3.cloud.upmem.com>
2022-04-25 04:50:04 +0200

    starting c code

--- upmem/src/load_generator.c
+++ upmem/src/load_generator.c
@@ -1,2 +1,23 @@
-include <>
+#include "common.h"
+#include "host/include/host.h"
+#include "emb_types.h"
+
+#include <assert.h>
+#include <dpu.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define NR_TABLES 10
+#define NR_COLS 64
+
+void synthetic_caller(){
+	for (int i=0; i<nr_batches; i++){
+		for (int j=0; j<indices_per_batch; j++){
+			
+		}
+	}
+}
+
 
