1d12e7d8d3fa35238176adeaf498aaa8361a9691
Niloofar Zarif <niloo@upmemcloud4.cloud.upmem.com>
2022-08-18 23:42:55 +0200

    Update

--- upmem/run.sh
+++ upmem/run.sh
@@ -41,9 +41,9 @@ kaggle_env() {
 build_pytorch=true
 random_env() {
     export NR_TABLES=10
-    export NR_COLS=32
-    export MAX_NR_BATCHES=64
-    export NR_TASKLETS=14
+    export NR_COLS=64
+    export MAX_NR_BATCHES=128
+    export NR_TASKLETS=10
 }
 
 # Old - pre-loadgenerator
@@ -87,21 +87,21 @@ kaggle_run() {
 random_run() {
     echo "Check env: NR_TABLES = ${NR_TABLES}, NR_COLS = ${NR_COLS}"
     if "${build_pytorch}"; then
-        cd ${cwd}/../PIM-Pytorch
+        cd ${cwd}/../../PIM-Pytorch
         # NR_TABLES=${NR_TABLES} NR_COLS=${NR_COLS} MAX_NR_BATCHES=${MAX_NR_BATCHES} NR_TASKLETS=${NR_TASKLETS} REL_WITH_DEB_INFO=1 DEBUG=1 USE_DISTRIBUTED=1 USE_MKLDNN=0 USE_CUDA=0 BUILD_TEST=0 USE_NNPACK=0 USE_QNNPACK=0 USE_XNNPACK=0 BUILD_CAFFE2=1 python3 "${cwd}/../PIM-Pytorch/setup.py" develop
         python3 setup.py clean
-        REL_WITH_DEB_INFO=1 DEBUG=1 USE_DISTRIBUTED=1 USE_MKLDNN=0 USE_CUDA=0 BUILD_TEST=0 USE_NNPACK=0 USE_QNNPACK=0 USE_XNNPACK=0 BUILD_CAFFE2=1 python3 "${cwd}/../PIM-Pytorch/setup.py" develop
+        REL_WITH_DEB_INFO=1 DEBUG=1 USE_DISTRIBUTED=1 USE_MKLDNN=0 USE_CUDA=0 BUILD_TEST=0 USE_NNPACK=0 USE_QNNPACK=0 USE_XNNPACK=0 BUILD_CAFFE2=1 python3 "${cwd}/../../PIM-Pytorch/setup.py" develop
     else
         echo "skipping pytorch build"
     fi
     cd "${cwd}/${build_dir}"
     python3 "${cwd}/../PIM-dlrm-new/dlrm_dpu_pytorch.py" \
-           --arch-embedding-size=65000-65000-65000 \
+           --arch-embedding-size=12000000-12000000-12000000-12000000-12000000-12000000-12000000-12000000-12000000-12000000 \
            --arch-sparse-feature-size=64 \
            --arch-mlp-bot=1440-720-64 \
            --arch-mlp-top=40-20-10-1 \
            --data-generation=random \
-           --mini-batch-size=32 \
+           --mini-batch-size=128 \
            --num-batches=100 \
            --num-indices-per-lookup=32 \
            --num-indices-per-lookup-fixed=True \
